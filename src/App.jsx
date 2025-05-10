8a2e4f35f181ee2054912bc1ac8e05f03e9fd83b
ShuckZ77 <iitbhu.rishish@gmail.com>
2025-05-10 22:53:14 +0530

    commit 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import DataTable from './components/DataTable';
-import { BrowserRouter } from "react-router-dom";
+// import { StrictMode, useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import DataTable from "./components/DataTable"
 // import './App.css'
 
 function App() {
   return (
-    <BrowserRouter basename="/CJR">
+    <>
       <DataTable />
-    </BrowserRouter>
+    </>
   );
 }
 
