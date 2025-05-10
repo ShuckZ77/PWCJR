315a107beb00e7330f89c72dc5fd17379a1aef7b
ShuckZ77 <iitbhu.rishish@gmail.com>
2025-05-10 22:58:27 +0530

    commit 3

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/CJR/", // your repo name with leading and trailing slash
+  base: "https://shuckz77.github.io/PWCJR/", // your repo name with leading and trailing slash
   plugins: [react()],
 });
